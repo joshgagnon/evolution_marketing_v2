2a99d3eb96bcd8a77dd9170d81769a1ee82198ce
Joshua Gagnon <josh.n.gagnon@gmail.com>
2016-06-13 11:47:25 +1200

    external links now in new tabs

--- src/javascripts/app.js
+++ src/javascripts/app.js
@@ -1,3 +1,7 @@
 import './modules'
 import 'jquery'
 import 'bootstrap-sass/assets/javascripts/bootstrap/collapse.js'
+
+[].map.call(document.querySelectorAll('a.external-link'), function(link){
+    link.target="_blank";
+});
